b46076d3b23a3f9ef4b25f62717c95fce5e28704
kidshay <shaygrant2009@hotmail.com>
2020-10-08 13:01:16 -0400

    Create automation .py
    first push

---
+++ automation .py
@@ -0,0 +1,34 @@
+import json 
+import os
+
+# Command to run to scan the domain
+
+os.system("subfinder -d hackerone.com -silent | httpx -silent | nuclei -t cves/ -o results.txt")
+
+
+
+  
+# the file to be converted to  
+# json format 
+filename = 'data.txt'
+  
+# dictionary where the lines from 
+# text will be stored 
+dict1 = {} 
+  
+# creating dictionary 
+with open(filename) as fh: 
+  
+    for line in fh: 
+  
+        # reads each line and trims of extra the spaces  
+        # and gives only the valid words 
+        command, description = line.strip().split(None, 1) 
+  
+        dict1[command] = description.strip() 
+  
+# creating json file 
+# the JSON file is named as test1 
+out_file = open("test1.json", "w") 
+json.dump(dict1, out_file, indent = 4, sort_keys = False) 
+out_file.close() 
